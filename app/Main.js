eb3d2c09a0a78541c25a7aea034442864ebad6c1
chule <cuckovic@gmail.com>
2016-12-15 15:38:30 +0100

    bg image added

--- app/Main.js
+++ app/Main.js
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Svg
 } from 'react-native';
 
-
+import backgroundImage from '../images/background.jpg';
 
 export default class Main extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Image style={styles.backgroundImage} source={backgroundImage}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!!!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+      </Image>  
     );
   }
 }
@@ -31,16 +35,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    //backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: '#ffffff',
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#ffffff',
     marginBottom: 5,
   },
+  backgroundImage: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: null,
+  }
+
 });
